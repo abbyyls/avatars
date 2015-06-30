d681a4287f4f5a26dfaa1d53a87d78c2cf2a193c
Anton Konovalov <antoni.konovalov@gmail.com>
2015-06-30 14:55:45 +0300

    feat(middleware): add cors

--- main.go
+++ main.go
@@ -17,12 +17,30 @@ var (
 	Listen = config.String("http", "0.0.0.0:4567")
 )
 
+// Options automatically return an appropriate "Allow" header when the
+// request method is OPTIONS and the request would have otherwise been 404'd.
+func Options(c *web.C, h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, api_key, Authorization")
+			w.Header().Set("Allow", "OPTIONS,HEAD,GET,POST,PUT,DELETE,OPTIONS")
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		h.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+}
+
 func main() {
 	config.SetPrefix("AV_")
 	config.Parse("")
 
 	mux := web.New()
 	mux.Use(middleware.Logger) // TODO: remove??
+	mux.Use(Options)
 
 	// NOTE: "RouterWithId" router needs for using URL parameters in CheckId middleware.
 	// Goji can't bind URL parameters until after the middleware stack runs.
